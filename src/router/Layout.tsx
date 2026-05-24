80b0987f60272832305ec3ac24d3f566426e84d1
miho19 <miho19@hotmail.co.nz>
2026-05-25 05:13:05 +1200

    moved sidebar into a seperate container

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,20 +1,11 @@
-import { Link, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
+import Sidebar from "../component/sidebar/Sidebar";
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="w-full p-3 flex items-center justify-between bg-white shadow-md">
-        <div className="flex flex-1">LOGO</div>
-        <div className="flex space-x-6">
-          <Link
-            to="/"
-            className="[&.active]:font-semibold border-black/15 hover:shadow-xl hover:scale-105 rounded-lg py-3 px-12 flex justify-center items-center transition-all duration-100 ease-in-out"
-          >
-            <p className="">Home</p>
-          </Link>
-        </div>
-
-        <div className="flex flex-1 justify-end">Notification</div>
+      <header className="w-full">
+        <Sidebar />
       </header>
       <Outlet />
     </div>
