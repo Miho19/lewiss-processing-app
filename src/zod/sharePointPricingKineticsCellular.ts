68c86db5d80f42c743eed3b60c3c65f56358fe0d
Miho19 <miho19@hotmail.co.nz>
2026-05-29 16:39:36 +1200

    kinetics cellular fabric price

---
+++ src/zod/sharePointPricingKineticsCellular.ts
@@ -0,0 +1,20 @@
+export type SharePointKineticsCellularPricingType = {
+  productId: string;
+  blockoutMultiplier: number;
+  sideChannelCustomColourSurcharge: number;
+  sideChannelCostPerMetreWidth: number;
+  headRailCustomColourSurcharge: number;
+  motorisation: Motorisation;
+  blind: Blind;
+};
+
+type Motorisation = {
+  "Lithium-ion": number;
+};
+
+type Blind = {
+  opacity: string;
+  heightHeader: number[];
+  widthHeader: number[];
+  data: number[][];
+};
