86507138b3ef2774f6736346d657b3fa5dab40d1
Miho19 <miho19@hotmail.co.nz>
2026-06-08 11:11:37 +1200

    getting sharepoint json items for pricing schedules

--- src/http/GETSharePointPricingSchedule.ts
+++ src/http/GETSharePointPricingSchedule.ts
@@ -1,6 +1,7 @@
 import type { BlindType } from "../zod/sharePointProjectFile";
 import kineticsCellularPricingSchedule from "../../test/utility/kinetics/cellular/kinetics-cellular-pricing-example.json";
 import type { SharePointKineticsCellularPricingType } from "../zod/kinetics/sharePointPricingKineticsCellular";
+import type { SharePointKineticsRollerPricingType } from "../zod/kinetics/sharePointPricingKineticsRoller";
 
 function GETSharePointPricingScheduleEndpoint() {
   return new URL(
@@ -19,15 +20,37 @@ function GETSharePointJSONFileFetchOptions(fileId: string): RequestInit {
   return fetchOptions;
 }
 
-type PricingScheduleType = SharePointKineticsCellularPricingType;
+type PricingScheduleType =
+  | SharePointKineticsCellularPricingType
+  | SharePointKineticsRollerPricingType;
 
-// we will use fixed names for the files
+type SharePointPricingScheduleItemIdRecordType = Record<BlindType, string>;
+
+const sharePointPricingScheduleRecord: SharePointPricingScheduleItemIdRecordType =
+  {
+    "Kinetics Mikronwood 50mm Venetian": "",
+    "Lewis's 25mm Aluminium Venetian": "",
+    "Lewis's 50mm Phoenixwood Venetian": "",
+    "Santa Fe Normandy Shutter": "",
+    "Santa Fe Waterproof Woodlore Plus Shutter": "",
+    "Kinetics Sunscreen Roller Blind": "01VFVMOABN3IZBHUHWK5D3GJ7VFKUZAHJL",
+    "Kinetics Blockout Roller Blind": "01VFVMOAGZEEB7JDTD7ZGZ5BJT2A4P6X7M",
+    "Kinetics Light Filtering Roller Blind":
+      "01VFVMOAGZEEB7JDTD7ZGZ5BJT2A4P6X7M",
+    "Kinetics 10mm Cellular Blind": "01VFVMOAHWC4LUAIEQDJC3ZSVTJ2Y65NY7",
+    "Kinetics 20mm Cellular Blind": "01VFVMOAHWC4LUAIEQDJC3ZSVTJ2Y65NY7",
+  };
 
 export async function GETSharePointPricingSchedule(
   blindType: BlindType,
   endpoint: URL = GETSharePointPricingScheduleEndpoint(),
 ): Promise<PricingScheduleType> {
   try {
+    const sharePointItemId: string | undefined =
+      sharePointPricingScheduleRecord[blindType];
+    if (typeof sharePointItemId === "undefined")
+      throw new Error(`Could not fetch pricing schedule for ${blindType}`);
+
     return kineticsCellularPricingSchedule;
   } catch (error) {
     if (error instanceof Error) console.error(error.message);
