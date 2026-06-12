f85a92ab3fc141e666cbe2a70ab7964112bc7b76
Miho19 <miho19@hotmail.co.nz>
2026-06-12 16:55:28 +1200

    additional product array for kinetics cellular

--- src/utility/kinetics/cellular/pricing/getKineticsCellularAdditionalProductList.ts
+++ src/utility/kinetics/cellular/pricing/getKineticsCellularAdditionalProductList.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import type { KineticsCellularPricingSchedule } from "../../../../type/pricing/kinetics/kineticsCellularPricingScheduleType";
 import type { ProcessName } from "../../../../type/process/processType";
 import type { BlindType } from "../../../../type/process/productType";
 import type { KineticsCellularTableEntry } from "../../../../type/process/tableEntry/kineticsTableEntryType";
 import type { AdditionalProduct } from "../../../../type/process/worksheetType";
+import { getPricingScheduleAsync } from "../../../process/pricingScheduleUtility";
 import { mapProcessNameToBlindType } from "../../../process/processUtility";
 
 // note that blindtype is going to be 10mm cellular regardless whether it is 10 or 20mm
@@ -19,5 +21,30 @@ export async function getKineticsCellularMotorAdditionalProductListAsync(
   tableEntryList: KineticsCellularTableEntry[],
   blindType: BlindType,
 ): Promise<AdditionalProduct[]> {
-  return [];
+  const operationRecord = new Map<string, number>();
+
+  tableEntryList.forEach((entry) => {
+    const current = operationRecord.get(entry.control) ?? 0;
+    operationRecord.set(entry.control, current + 1);
+  });
+
+  const pricingSchedule = (await getPricingScheduleAsync(
+    blindType,
+  )) as KineticsCellularPricingSchedule;
+  if (typeof pricingSchedule === "undefined") return [];
+
+  const motorisationObject = pricingSchedule.control.motorisation;
+
+  const options: AdditionalProduct[] = (
+    Object.keys(motorisationObject) as string[]
+  ).map((key) => {
+    const quantity = operationRecord.get(key) ?? 0;
+    const cost =
+      motorisationObject[key as keyof typeof motorisationObject].base;
+    return { name: key, cost: cost, quantity: quantity };
+  });
+
+  const filtered = options.filter((p) => p.quantity !== 0);
+
+  return filtered;
 }
