e53ddd779b9adff470898af0c1e78cd391127593
Miho19 <miho19@hotmail.co.nz>
2026-06-26 16:35:12 +1200

    fabric cost calc for mikronwood

---
+++ src/utility/kinetics/mikronwood/pricing/getKineticsMikronwoodFabricCost.ts
@@ -0,0 +1,7 @@
+import type { PricingSchedule } from "../../../../type/process/pricingScheduleType";
+
+export async function getKineticsMikronwoodFabricCost(
+  width: number,
+  height: number,
+  pricingSchedule: PricingSchedule,
+) {}
